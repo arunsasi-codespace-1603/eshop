845ee7621bf1da0899282a79c486e4efff8e3495
arunsasi-49 <arunsasi1603@gmail.com>
2026-09-04 10:36:31 +0100

    Footer accordion section fot mobile devices

--- src/components/ui/Footer/Footer.jsx
+++ src/components/ui/Footer/Footer.jsx
@@ -1,7 +1,17 @@
 import "./Footer.scss";
 
+// React Core
 import { useState } from "react";
+
+// Icons
+import {
+    ChevronDown,
+    ChevronRight
+} from "react-bootstrap-icons";
+
+// Custom Hook
 import useIsMobile from "../../../hooks/useIsMobile";
+
 const footerData = [
     {
         id: 1,
@@ -128,61 +138,102 @@ const footerData = [
         ]
     }
 ];
+
 const Footer = () => {
     const isMobileDevice = useIsMobile();
+
     const [activeAccordion, setActiveAccordion] = useState(null);
 
-    const toggleAccordion = (index) => {
-        console.log(index)
-        setActiveAccordion(prev =>
-            prev === index ? null : index
-        );
-    }
+    const toggleAccordion = (id) => {
+        setActiveAccordion((prev) => {
+            if (prev === id) {
+                return null;
+            }
+
+            return id;
+        });
+    };
+
     return (
         <footer className="footer">
-            <div className="container">
-                <div className="row">
-                    <div className="footer__container">
-                        {footerData.map((section) => (
-                            < div className="footer__column" key={section.id} >
+            <div className="container-fluid">
 
+                {isMobileDevice &&
+                    <div className="footer__logo">
+                        Logo
+                    </div>
+                }
+                <div className="footer__container">
 
-                                <div className="footer-categ">
-                                    <div className="footer-categ__header"
-                                        onClick={isMobileDevice ? () => toggleAccordion(section.id) : undefined}>
+                    {footerData.map((section) => (
+                        <div
+                            className="footer__column"
+                            key={section.id}
+                        >
+                            <div className="footer-categ">
+
+                                <div
+                                    className="footer-categ__header"
+                                    onClick={() => {
+                                        if (isMobileDevice) {
+                                            toggleAccordion(section.id);
+                                        }
+                                    }}
+                                >
+                                    <span>
                                         {section.title}
-                                    </div>
-                                    <div className={`footer-categ__body ${isMobileDevice && activeAccordion === section.id ? "active" : ""
-                                        }`}>
+                                    </span>
 
-                                        <ul className="categ-list">
-                                            {section.items.map((item, index) => (
-                                                <li className="categ-list__item" key={index}>
-                                                    {item.type === "link" ? (
-                                                        <a
-                                                            href={item.href}
-                                                            className="categ-list__link"
-                                                        >
-                                                            {item.label}
-                                                        </a>
-                                                    ) : (
-                                                        <div className="categ-list__block">
-                                                            {item.content}
-                                                        </div>
-                                                    )}
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </div>
+                                    {isMobileDevice && (
+                                        <div className="icon">
+                                            {activeAccordion === section.id ? (
+                                                <ChevronDown />
+                                            ) : (
+                                                <ChevronRight />
+                                            )}
+                                        </div>
+                                    )}
+                                </div>
+
+                                <div
+                                    className={`footer-categ__body ${activeAccordion === section.id
+                                        ? "active"
+                                        : ""
+                                        }`}
+                                >
+                                    <ul className="categ-list">
+
+                                        {section.items.map((item, index) => (
+                                            <li
+                                                className="categ-list__item"
+                                                key={index}
+                                            >
+                                                {item.type === "link" ? (
+                                                    <a
+                                                        href={item.href}
+                                                        className="categ-list__link"
+                                                    >
+                                                        {item.label}
+                                                    </a>
+                                                ) : (
+                                                    <div className="categ-list__block">
+                                                        {item.content}
+                                                    </div>
+                                                )}
+                                            </li>
+                                        ))}
+
+                                    </ul>
                                 </div>
 
                             </div>
-                        ))}
+                        </div>
+                    ))}
 
-                    </div>
                 </div>
             </div>
-        </footer >
-    )
-}
-export default Footer
+        </footer>
+    );
+};
+
+export default Footer;
